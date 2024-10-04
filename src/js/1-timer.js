08b6c927ebe2cfcb14a44c01d54c673c0b95a3d4
Oleh18zb <110353014+Oleh18zb@users.noreply.github.com>
2024-10-04 15:42:29 +0200

    видалив зайві підключення бібліотек
    та підключив за допомогою npm

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,17 +1,7 @@
-
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
-
-// // If using flatpickr in a framework, its recommended to pass the element directly
-// flatpickr(element, {});
-
-// // Otherwise, selectors are also supported
-// flatpickr("#myID", {});
-
-// // creates multiple instances
-// flatpickr(".anotherSelector");
 
 const dateTime = document.getElementById('datetime-picker');
 const startBtn = document.querySelector('button[data-start]');
